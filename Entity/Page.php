fe389e2c7c994dec74946c8126b151fba2885f2f
Jonas Flodén <jonas@koalasoft.se>
2012-05-01 16:04:45 +0200

    Remove title from Page entity

--- Entity/Page.php
+++ Entity/Page.php
@@ -15,9 +15,7 @@ class Page
 	protected $id;
 
 	/**
-	 * @ORM\Column(type="string")
 	 */
-	protected $title;
 
 	/**
 	 * @ORM\Column(type="string", length=100)
@@ -52,26 +50,6 @@ class Page
 	public function getId()
 	{
 		return $this->id;
-	}
-
-	/**
-	 * Set title
-	 *
-	 * @param string $title
-	 */
-	public function setTitle($title)
-	{
-		$this->title = $title;
-	}
-
-	/**
-	 * Get title
-	 *
-	 * @return string
-	 */
-	public function getTitle()
-	{
-		return $this->title;
 	}
 
 	/**
