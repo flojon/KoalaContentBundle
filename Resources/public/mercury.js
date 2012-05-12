f532d00fdd2230270a02e11e1d93bac5ae67205d
Jonas Flodén <jonas@koalasoft.se>
2012-05-12 11:26:58 +0200

    Set the Mercury file upload URL through meta tag

--- Resources/public/mercury.js
+++ Resources/public/mercury.js
@@ -1,3 +1,10 @@
+if (top.Mercury) {
+  window.Mercury = top.Mercury;
+  Mercury.on('ready', function() {
+	  Mercury.config.uploading.url = top.$('meta[name=mercury-images]').attr('content');
+  });
+}
+
 // Enable Mercury after clicking in a mercury region
 top.$(document).one('click', ".mercury-region-preview", function(event) {
   Mercury.trigger('toggle:interface');
