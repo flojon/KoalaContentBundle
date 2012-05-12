ff92fbda2838143137be28fd5617f64081f444ef
Jonas Flodén <jonas@koalasoft.se>
2012-05-12 11:24:05 +0200

    Remove listener after toggle:interface
    The listener causes troubles with simple regions which loses focus.
    It's only needed one time anyway…

--- Resources/public/mercury.js
+++ Resources/public/mercury.js
@@ -1,4 +1,4 @@
 // Enable Mercury after clicking in a mercury region
-top.$(document).on('click', ".mercury-region-preview", function(event) {
+top.$(document).one('click', ".mercury-region-preview", function(event) {
   Mercury.trigger('toggle:interface');
 });
