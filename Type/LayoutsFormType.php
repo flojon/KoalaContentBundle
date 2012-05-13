cf764173e1e2f8c2024119036f714dfb5f71466d
Jonas Flodén <jonas@koalasoft.se>
2012-05-13 15:50:38 +0200

    Add a new LayoutsFormType which uses the LayoutsProvider service

---
+++ Type/LayoutsFormType.php
@@ -0,0 +1,31 @@
+<?php
+namespace Koala\ContentBundle\Type;
+
+use Symfony\Component\Form\AbstractType;
+
+class LayoutsFormType extends AbstractType
+{
+    var $provider;
+
+    public function __construct($provider)
+    {
+        $this->provider = $provider;
+    }
+    
+    public function getDefaultOptions(array $options)
+    {
+        return array(
+            'choices' => $this->provider->getChoices(),
+        );
+    }
+
+    public function getParent(array $options)
+    {
+        return 'choice';
+    }
+
+    public function getName()
+    {
+        return 'layout';
+    }
+}
