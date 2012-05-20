0674bd984f78db26ec8cda52dfae9bb8b29e4759
Jonas Flodén <jonas@koalasoft.se>
2012-05-20 17:59:14 +0200

    Load custom CSS file

--- Resources/public/js/mercury_package.js
+++ Resources/public/js/mercury_package.js
@@ -1,6 +1,6 @@
 window.mercuryPackages = {
     bundled: {
         javascripts: 'javascripts/mercury.min.js,javascripts/mercury_dialogs.js,../bundles/koalacontent/js/mercury_plugin.js',
-        stylesheets: 'stylesheets/mercury.bundle.css'
+        stylesheets: 'stylesheets/mercury.bundle.css,../bundles/koalacontent/css/mercury_plugin.css'
     }
 };  
