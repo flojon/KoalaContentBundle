ef21ce37f2b9d4089447e99f422763f7628d18db
Jonas Flodén <jonas@koalasoft.se>
2012-05-20 17:55:07 +0200

    Populate Page select through NestedTreeRepository which gives the right order

--- Type/PageType.php
+++ Type/PageType.php
@@ -3,12 +3,20 @@ namespace Koala\ContentBundle\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
+use Gedmo\Tree\Entity\Repository\NestedTreeRepository;
 
 class PageType extends AbstractType
 {
 	public function buildForm(FormBuilder $builder, array $options)
 	{
-		$builder->add('parent', 'entity', array('class'=>'KoalaContentBundle:Page', 'empty_value'=>'-- No parent --'));
+        $builder->add('parent', 'entity', array(
+            'class'=>'KoalaContentBundle:Page',
+            'query_builder' =>
+                function(NestedTreeRepository $er) {
+                    return $er->childrenQueryBuilder();
+                }
+            )
+        );
 		$builder->add('menuTitle');
 		$builder->add('url');
 		$builder->add('slug');
