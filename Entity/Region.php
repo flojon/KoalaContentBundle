829d9c8c737815d0e89921ccd6ceb5867f79b09a
Jonas Flodén <jonas@koalasoft.se>
2012-08-10 11:18:28 +0200

    Create base model files for Page and Region in preparation for PHPCR

--- Entity/Region.php
+++ Entity/Region.php
@@ -1,83 +1,8 @@
 <?php
 namespace Koala\ContentBundle\Entity;
 
-class Region
+use Koala\ContentBundle\Model\Region as BaseRegion;
+
+class Region extends BaseRegion
 {
-    protected $id;
-
-    protected $name;
-
-    protected $content;
-
-    protected $page;
-
-    /**
-     * Get id
-     *
-     * @return integer
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set name
-     *
-     * @param string $name
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
-
-    /**
-     * Get name
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * Set content
-     *
-     * @param text $content
-     */
-    public function setContent($content)
-    {
-        $this->content = $content;
-    }
-
-    /**
-     * Get content
-     *
-     * @return text
-     */
-    public function getContent()
-    {
-        return $this->content;
-    }
-
-    /**
-     * Set page
-     *
-     * @param Koala\ContentBundle\Entity\Page $page
-     */
-    public function setPage(\Koala\ContentBundle\Entity\Page $page)
-    {
-        $this->page = $page;
-    }
-
-    /**
-     * Get page
-     *
-     * @return Koala\ContentBundle\Entity\Page
-     */
-    public function getPage()
-    {
-        return $this->page;
-    }
 }
